9dd04d0b7e6dedf09d75fe446156d3cc4e525362
AlexPHPower <bearhug@alexpower.co.uk>
2023-06-21 09:12:16 +0100

    Added file upload controller

---
+++ app/Http/Controllers/Uploader/FileUploadController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Uploader;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Uploader\FileUploadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,4 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', HomeController::class)->name('home');
+Route::post('/upload', FileUploadController::class)->name('upload.file');
